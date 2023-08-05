125ee227e144597a036854d21d604a229a850992
PrashantTapare198 <141405748+PrashantTapare198@users.noreply.github.com>
2023-08-05 17:39:11 +0530

    Update Demo4.java

--- GitProject/src/Vpt/Demo4.java
+++ GitProject/src/Vpt/Demo4.java
@@ -3,5 +3,6 @@ package Vpt;
 public class Demo4 {
 public static void main(String[] args) {
 	System.out.println("This is exampal of pull And Push");
+	System.out.println("This pull Concept Edited in gitpage")
 }
 }
