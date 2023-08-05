785069074ddc8f3e9575117b0a19910402e60314
Dell <Dell@DESKTOP-IVU6264>
2023-08-05 17:41:16 +0530

    by p

--- GitProject/src/Vpt/Demo4.java
+++ GitProject/src/Vpt/Demo4.java
@@ -3,6 +3,6 @@ package Vpt;
 public class Demo4 {
 public static void main(String[] args) {
 	System.out.println("This is exampal of pull And Push");
-	System.out.println("This pull Concept Edited in gitpage")
+	System.out.println("This pull Concept Edited in gitpage");
 }
 }
